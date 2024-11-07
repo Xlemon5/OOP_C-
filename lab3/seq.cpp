c6242ebe833311c345576afa205c376ac1b585e8
Xlemon5 <80254502+Xlemon5@users.noreply.github.com>
2024-11-07 18:36:31 +0300

    Update seq.cpp

--- lab3/seq.cpp
+++ lab3/seq.cpp
@@ -4,39 +4,45 @@
 #include <ctime>  // Для измерения времени
 #include <omp.h>
 #include "windows.h" 
+#define NUM_THREADS 1
 
 struct Point {
     double x, y, z;
 };
 
-void transform_points(std::vector<Point>& points, double offset_x, double offset_y, double offset_z) {
-    for (auto& point : points) {
-        point.x += offset_x;
-        point.y += offset_y;
-        point.z += offset_z;
+
+void transform_points_parallel(std::vector<Point>& points, double offset_x, double offset_y, double offset_z) {
+    int num_points = points.size();
+    omp_set_num_threads(NUM_THREADS);
+#pragma omp parallel for
+    for (int i = 0; i < num_points; ++i) {
+        points[i].x += offset_x;
+        points[i].y += offset_y;
+        points[i].z += offset_z;
     }
+#pragma omp barrier
 }
 
 int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    const int num_points = 100; // Количество точек
-    std::vector<Point> points(num_points); // Создание вектора точек
-
+    const int num_points = 100000000;
+    std::vector<Point> points(num_points);
 
     for (int i = 0; i < num_points; ++i) {
         points[i] = { double(i), double(i), double(i) }; // Каждая точка инициализируется значениями i
     }
 
+
     double offset_x = 1.0, offset_y = 2.0, offset_z = 3.0;
+
 
     double start_time = omp_get_wtime(); // Засекаем начальное время
 
-    transform_points(points, offset_x, offset_y, offset_z);
+    transform_points_parallel(points, offset_x, offset_y, offset_z);
 
     double end_time = omp_get_wtime(); // Засекаем конечное время
 
-    double elapsed_time = double(end_time - start_time);// Время выполнения в секундах
+
+    double elapsed_time = end_time - start_time; // Время выполнения в секундах
 
     std::cout << "Sequential Time: " << std::fixed << std::setprecision(6) << elapsed_time << " seconds\n";
 
